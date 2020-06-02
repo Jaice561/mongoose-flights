49bbab50c6fbf70caebd7fe8df8399e87f8afc29
Jaice561 <Jameson_exil@yahoo.com>
2020-06-02 14:55:08 -0500

    !

--- routes/destinations.js
+++ routes/destinations.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var destinationsCtrl = require('../controllers/destinations');
 
-router.post('/flights/:id/destinations', destinationsCtrl.create);
+router.post('/flights/:id', destinationsCtrl.create);
 
 module.exports = router;
